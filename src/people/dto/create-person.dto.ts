2c4c4cdc7b2a3532f4703a9a2fe570407e73b57b
Bruna Nunes <bruna.f.n@hotmail.com>
2025-02-23 12:03:20 +0000

    ajustes limite de memoria devcontainer e dtos de pessoas

--- src/people/dto/create-person.dto.ts
+++ src/people/dto/create-person.dto.ts
@@ -1,48 +1,52 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Company } from 'src/companies/entities/company.entity';
 
 export class CreatePersonDto {
-    @ApiProperty()
-    id: number;
+  @ApiProperty()
+  id: number;
 
-    @ApiProperty({ maxLength: 50 })
-    name: string;
+  @ApiProperty({ maxLength: 50 })
+  name: string;
 
-    @ApiProperty({ maxLength: 20, required: false })
-    phone: string;
+  @ApiProperty({ maxLength: 20, required: false })
+  phone: string;
 
-    @ApiProperty({ maxLength: 100, required: false })
-    email: string;
+  @ApiProperty({ maxLength: 100, required: false })
+  email: string;
 
-    @ApiProperty({ maxLength: 11, required: false })
-    cpf: string;
+  @ApiProperty({ maxLength: 11, required: false })
+  cpf: string;
 
-    @ApiProperty({ required: false })
-    born_date: Date;
+  @ApiProperty({ required: false })
+  born_date: Date;
 
-    @ApiProperty({ maxLength: 9, required: false })
-    cep: string;
+  @ApiProperty({ maxLength: 9, required: false })
+  cep: string;
 
-    @ApiProperty({ maxLength: 100, required: false })
-    street: string;
+  @ApiProperty({ maxLength: 100, required: false })
+  street: string;
 
-    @ApiProperty({ maxLength: 10, required: false })
-    number: string;
+  @ApiProperty({ maxLength: 10, required: false })
+  number: string;
 
-    @ApiProperty({ maxLength: 50, required: false })
-    city: string;
+  @ApiProperty({ maxLength: 50, required: false })
+  city: string;
 
-    @ApiProperty({ maxLength: 50, required: false })
-    neighborhood: string;
+  @ApiProperty({ maxLength: 50, required: false })
+  neighborhood: string;
 
-    @ApiProperty({ maxLength: 2, required: false })
-    uf: string;
+  @ApiProperty({ maxLength: 2, required: false })
+  uf: string;
 
-    @ApiProperty()
-    status: boolean;
+  @ApiProperty()
+  status: boolean;
 
-    @ApiProperty({ type: 'string', default: () => 'CURRENT_TIMESTAMP' })
-    created_at: Date;
+  @ApiProperty({ type: 'string', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
 
-    @ApiProperty({ type: 'string', default: () => 'CURRENT_TIMESTAMP' })
-    updated_at: Date;
+  @ApiProperty({ type: 'string', default: () => 'CURRENT_TIMESTAMP' })
+  updated_at: Date;
+
+  @ApiProperty({ type: [Company], required: false })
+  companies: Company[];
 }
--- src/people/dto/update-person.dto.ts
+++ src/people/dto/update-person.dto.ts
@@ -1,40 +1,4 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreatePersonDto } from './create-person.dto';
 
-export class UpdatePersonDto extends PartialType(CreatePersonDto) {
-  @ApiProperty({ maxLength: 50 })
-  name: string;
-
-  @ApiProperty({ maxLength: 20, required: false })
-  phone: string;
-
-  @ApiProperty({ maxLength: 100, required: false })
-  email: string;
-
-  @ApiProperty({ maxLength: 11, required: false })
-  cpf: string;
-
-  @ApiProperty({ required: false })
-  born_date: Date;
-
-  @ApiProperty({ maxLength: 9, required: false })
-  cep: string;
-
-  @ApiProperty({ maxLength: 100, required: false })
-  street: string;
-
-  @ApiProperty({ maxLength: 10, required: false })
-  number: string;
-
-  @ApiProperty({ maxLength: 50, required: false })
-  city: string;
-
-  @ApiProperty({ maxLength: 50, required: false })
-  neighborhood: string;
-
-  @ApiProperty({ maxLength: 2, required: false })
-  uf: string;
-
-  @ApiProperty()
-  status: boolean;
-}
+export class UpdatePersonDto extends PartialType(CreatePersonDto) {}
