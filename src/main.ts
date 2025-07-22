f1a22cf18fe804231c4da20c2ebfe239370b8f62
Bruna Nunes <bruna.f.n@hotmail.com>
2025-07-22 00:03:30 -0300

    ajuste cors

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
 app.enableCors({
-  origin: ['https://sistemamarcapranos-stable.up.railway.app/', 'https://marcapranos.up.railway.app/', 'https://sistema.marcapranos.com.br/', 'https://marcapranos.com.br/', 'http://localhost:5173'],
+  origin: ['https://sistemamarcapranos-stable.up.railway.app', 'https://marcapranos.up.railway.app', 'https://sistema.marcapranos.com.br', 'https://marcapranos.com.br', 'http://localhost:5173'],
   methods: 'GET,POST,PUT,DELETE',
 });
 
